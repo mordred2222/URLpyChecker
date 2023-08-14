160f64113fae17b62201d4091c884be5ca65e99d
Roland Van Puyenbroeck <5094960+mordred2222@users.noreply.github.com>
2023-08-14 13:05:57 +0200

    Create CheckURLs.py
    The main python file

---
+++ CheckURLs.py
@@ -0,0 +1,32 @@
+import openpyxl
+import requests
+
+# Path to the excel file 
+excelFilePath = "C:\\BRG\\URL.xlsx"
+
+# Name of excel sheet tab
+worksheetName = "tocheck"
+
+# Load the excel file
+workbook = openpyxl.load_workbook(excelFilePath)
+
+# Select the excel source tab 
+worksheet = workbook[worksheetName]
+
+# Create a new excel tab to put result , existing tab renamed
+checkedWorksheetName = "checked"
+checkedWorksheet = workbook.create_sheet(title=checkedWorksheetName)
+
+# Parcourir chaque ligne de l'onglet et vérifier les URL
+for row in worksheet.iter_rows(min_row=2, values_only=True):  # Commence à la deuxième ligne pour ignorer les en-têtes
+    url = row[1]  # Remplacez l'indice par la colonne souhaitée (0-based index)
+    response = requests.get(url)
+
+    result = "Accessible" if 200 <= response.status_code < 400 else "Erreur "+str(response.status_code)
+
+    checkedWorksheet.append([url, result])
+
+# Sauvegarder les modifications dans le fichier Excel
+workbook.save(excelFilePath)
+
+print(f"Résultats stockés dans l'onglet '{checkedWorksheetName}' du fichier '{excelFilePath}'.")
